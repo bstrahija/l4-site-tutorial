c612042205c489d3af3c973cbbaa59917eb1373e
Boris Strahija <boris@creolab.hr>
2013-05-24 09:21:39 +0200

    Added a homepage view.

---
+++ app/views/hello.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel 4 Tutorial</title>
+
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>Hello world!</h1>
+<p>Nothing to see here, <a href="{{ URL::route('admin.login') }}">proceed to login</a>.</p>
+
+</body>
+</html>
+
