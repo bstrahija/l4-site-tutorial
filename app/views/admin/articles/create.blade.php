e9a2a7f5eeac15b2fb0a9bb861d9c476639ada3e
Boris Strahija <boris@creolab.hr>
2013-07-05 14:49:05 +0200

    Added upload elements

--- app/views/admin/articles/create.blade.php
+++ app/views/admin/articles/create.blade.php
@@ -24,8 +24,15 @@
 
 		<div class="control-group">
 			{{ Form::label('image', 'Image') }}
-			<div class="controls">
-				{{ Form::file('image') }}
+
+			<div class="fileupload fileupload-new" data-provides="fileupload">
+				<div class="fileupload-preview thumbnail" style="width: 200px; height: 150px;">
+					<img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image">
+				</div>
+				<div>
+					<span class="btn btn-file"><span class="fileupload-new">Select image</span><span class="fileupload-exists">Change</span>{{ Form::file('image') }}</span>
+					<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
+				</div>
 			</div>
 		</div>
 
--- app/views/admin/articles/edit.blade.php
+++ app/views/admin/articles/edit.blade.php
@@ -24,13 +24,20 @@
 
 		<div class="control-group">
 			{{ Form::label('image', 'Image') }}
-			<div class="controls">
-				{{ Form::file('image') }}
-			</div>
 
-			<?php if ($article->image) : ?>
-				<a href="<?php echo $article->image; ?>"><img src="<?php echo Image::thumb($article->image, 80); ?>" alt=""></a>
-			<?php endif; ?>
+			<div class="fileupload fileupload-new" data-provides="fileupload">
+				<div class="fileupload-preview thumbnail" style="width: 200px; height: 150px;">
+					@if ($article->image)
+						<a href="<?php echo $article->image; ?>"><img src="<?php echo Image::resize($article->image, 200, 150); ?>" alt=""></a>
+					@else
+						<img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image">
+					@endif
+				</div>
+				<div>
+					<span class="btn btn-file"><span class="fileupload-new">Select image</span><span class="fileupload-exists">Change</span>{{ Form::file('image') }}</span>
+					<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
+				</div>
+			</div>
 		</div>
 
 		<div class="form-actions">
