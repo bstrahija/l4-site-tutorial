d0bf0ce0189c5f821b7e5f3d30fa3b2ddaeb53e3
Ionut Tanasa <ionutz2k@gmail.com>
2013-09-13 15:00:14 +0200

    Configured Database

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'starter',
+			'database'  => 'tester',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
