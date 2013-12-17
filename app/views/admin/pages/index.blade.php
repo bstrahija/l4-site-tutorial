755e588c8daaeb2336bbfd90ee77c3a7ec6d97a1
Boris Strahija <boris@creolab.hr>
2013-12-17 09:56:39 +0100

    Fixed type

--- app/views/admin/pages/index.blade.php
+++ app/views/admin/pages/index.blade.php
@@ -29,7 +29,7 @@
 						<a href="{{ URL::route('admin.pages.edit', $page->id) }}" class="btn btn-success btn-mini pull-left">Edit</a>
 
 						{{ Form::open(array('route' => array('admin.pages.destroy', $page->id), 'method' => 'delete', 'data-confirm' => 'Are you sure?')) }}
-							<button type="submit" href="{{ URL::route('admin.pages.destroy', $page->id) }}" class="btn btn-danger btn-mini">Delete</butfon>
+							<button type="submit" href="{{ URL::route('admin.pages.destroy', $page->id) }}" class="btn btn-danger btn-mini">Delete</button>
 						{{ Form::close() }}
 					</td>
 				</tr>
